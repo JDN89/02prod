190b0ed07962dce5d61b696cbe65b2e6ee8bbd73
JDN89 <jan.de.niels@gmail.com>
2023-02-15 23:36:35 +0100

    feat: setup psql in docker + sqlx

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web:: get().to(health_check))
+            .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
