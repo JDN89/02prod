0ff43ee25ff26bd6597d942ed26fa7fee4b70ee3
JDN89 <jan.de.niels@gmail.com>
2023-03-04 12:42:53 +0100

    setting up a docker container

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 //! src/main.rs
 use std::net::TcpListener;
 
-use env_logger::Env;
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
-use tracing::Subscriber;
+use sqlx::postgres::PgPoolOptions;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -19,7 +17,7 @@ async fn main() -> std::io::Result<()> {
     LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or(EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
+   let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
         std::io::stdout,
     );
@@ -30,10 +28,11 @@ async fn main() -> std::io::Result<()> {
     set_global_default(subscriber).expect("Failed to set subscriber");
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host,configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
